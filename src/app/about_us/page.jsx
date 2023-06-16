11237bfd6e038ed64a5c54edc7ddb0300e697fed
Sulayman <sirikone@hotmail.com>
2023-06-16 18:39:26 +0200

    test debugg

--- src/app/about_us/page.jsx
+++ src/app/about_us/page.jsx
@@ -9,15 +9,14 @@ export default function AboutUs() {
             <title>À propos de nous</title>
             <h1 className="titre">À propos de nous</h1>
             <article>
-                <p className="first-line">Order App Aid est une application de commande de nourriture pour les événements, tels que l'Aid.
-                </p>
-                <p>Cette application facilite la prise de commande de nourriture en flashant le QR code placé sur les tables. Les commandes seront envoyées à une imprimante qui imprimera des tickets de commande similaires à ceux d'un fast-food.</p>
+                <p className="first-line">Order App Aid est une application de commande de nourriture pour les événements, tels que l'Aid.</p>
+                {/* <p>Cette application facilite la prise de commande de nourriture en flashant le QR code placé sur les tables. Les commandes seront envoyées à une imprimante qui imprimera des tickets de commande similaires à ceux d'un fast-food.</p> */}
                 <p>Nous sommes dévoués à offrir une expérience de commande simple et pratique pour les clients lors de vos événements spéciaux. Notre application vous permet de parcourir facilement le menu, de passer des commandes et de les recevoir rapidement.</p>
                 <p>Que vous organisiez une grande fête, un événement corporatif ou une réunion de famille, notre application de commande de nourriture vous aide à rationaliser le processus et à vous assurer que vos invités sont satisfaits de leurs repas.</p>
                 <p>Nous travaillons en étroite collaboration avec des restaurateurs locaux de confiance pour vous offrir une sélection variée de plats délicieux. Chaque commande est traitée avec soin et préparée avec des ingrédients frais et de qualité.</p>
-                <p>Nous sommes impatients de vous servir et de contribuer à la réussite de vos événements. Utilisez l'application Order App Aid pour rendre votre prochain événement encore plus spécial!</p>
+                {/* <p>Nous sommes impatients de vous servir et de contribuer à la réussite de vos événements. Utilisez l'application Order App Aid pour rendre votre prochain événement encore plus spécial!</p> */}
                 <p>Merci de nous faire confiance pour vos besoins en matière de commande de nourriture.</p>
-                <p className="credits">L'équipe Order App Aid</p>
+                <p className="credits">L\'équipe Order App Aid</p>
             </article>
         </main>
     )
