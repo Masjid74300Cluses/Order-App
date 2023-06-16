b57754c000174f6811dcbfeca64353e7bf6498b8
Sulayman <sirikone@hotmail.com>
2023-06-17 00:13:14 +0200

    debugg 2

--- src/app/about_us/page.jsx
+++ src/app/about_us/page.jsx
@@ -9,16 +9,7 @@ export default function AboutUs() {
             <title>À propos de nous</title>
             <h1 className="titre">À propos de nous</h1>
             <article>
-                <p>
-                    Order App Aid est une application de commande de nourriture pour les événements, tels que l'Aid.
-
-                    Cette application facilite la prise de commande de nourriture en flashant le QR code placé sur les tables. Les commandes seront envoyées à une imprimante qui imprimera des tickets de commande similaires à ceux d'un fast-food.
-                    Nous sommes dévoués à offrir une expérience de commande simple et pratique pour les clients lors de vos événements spéciaux. Notre application vous permet de parcourir facilement le menu, de passer des commandes et de les recevoir rapidement.
-                    Que vous organisiez une grande fête, un événement corporatif ou une réunion de famille, notre application de commande de nourriture vous aide à rationaliser le processus et à vous assurer que vos invités sont satisfaits de leurs repas.
-                    Nous travaillons en étroite collaboration avec des restaurateurs locaux de confiance pour vous offrir une sélection variée de plats délicieux. Chaque commande est traitée avec soin et préparée avec des ingrédients frais et de qualité.
-                    Nous sommes impatients de vous servir et de contribuer à la réussite de vos événements. Utilisez l'application Order App Aid pour rendre votre prochain événement encore plus spécial!
-                    Merci de nous faire confiance pour vos besoins en matière de commande de nourriture.
-                    L'équipe Order App Aid</p>
+                Nous sommes heureux de vous aider à éviter les queues inutilement, asseyez vous tranquillement vous serrez servis à table après avoir flashé le qr code et payé en toute sécurité.
             </article>
         </main>
     )
