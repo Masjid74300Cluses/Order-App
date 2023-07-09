4a3b43d9a41bfb4ce4352ac2e425350ad3f5fb22
Farouk Mekkaoui <farouk.mekkaoui@gmail.com>
2023-07-09 10:03:24 +0200

    change print url

--- src/app/api/order/route.js
+++ src/app/api/order/route.js
@@ -1,52 +1,51 @@
-import {NextResponse} from "next/server";
+import { NextResponse } from "next/server";
 
 const sendConfirmationEmail = (commande) => {
-    // Requête pour envoyer l'e-mail de confirmation de commande
-    return fetch('/api/send-email', {
-        method: 'POST',
-        body: JSON.stringify({ commande }),
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
+  // Requête pour envoyer l'e-mail de confirmation de commande
+  return fetch("/api/send-email", {
+    method: "POST",
+    body: JSON.stringify({ commande }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 };
 
 const storeOrderData = (commande) => {
-    // Requête pour stocker les informations de commande en base de données
-    return fetch('/api/store-order', {
-        method: 'POST',
-        body: JSON.stringify({ commande }),
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
+  // Requête pour stocker les informations de commande en base de données
+  return fetch("/api/store-order", {
+    method: "POST",
+    body: JSON.stringify({ commande }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 };
 
 const printTicket = (commande) => {
-    // Requête pour imprimer le ticket thermique
-    return fetch('https://5918-2a01-e0a-871-62f0-599c-b509-b92b-ea9b.ngrok-free.app/print', {
-        method: 'POST',
-        body: JSON.stringify({ title: 'Envoie depuis prod', products:commande }),
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Basic '+btoa('admin:admin'), 
-        },
-    });
+  // Requête pour imprimer le ticket thermique
+  return fetch("https://b7d456b380f1.ngrok.app/print", {
+    method: "POST",
+    body: JSON.stringify({ title: "Envoie depuis prod", products: commande }),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Basic " + btoa("admin:admin"),
+    },
+  });
 };
 
-
 export async function POST(req, res) {
-    const { items } = req.body;
-    // save order  get number send email
-    /*const responseEmail = await sendConfirmationEmail(formData);
+  const { items } = req.body;
+  // save order  get number send email
+  /*const responseEmail = await sendConfirmationEmail(formData);
     const responseStore = await storeOrderData(formData);*/
-    const products = [
-      { name: 'Produit 1', price: 100, quantity: 2 },
-      { name: 'Produit 2', price: 150, quantity: 1 },
-      { name: 'Produit 3', price: 80, quantity: 3 }
-    ];
-    const responsePrint = await printTicket(products);
-    return NextResponse.json({
-        items: JSON.stringify(items)
-    });
+  const products = [
+    { name: "Produit 1", price: 100, quantity: 2 },
+    { name: "Produit 2", price: 150, quantity: 1 },
+    { name: "Produit 3", price: 80, quantity: 3 },
+  ];
+  const responsePrint = await printTicket(products);
+  return NextResponse.json({
+    items: JSON.stringify(items),
+  });
 }
